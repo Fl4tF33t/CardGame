983341022ed68c255b5a35df7e7c20a6e2e401c4
Fl4tF33t <89871056+Fl4tF33t@users.noreply.github.com>
2022-03-10 22:09:54 +0200

    its working +1mana

--- Assets/Scripts/PlayZone.cs
+++ Assets/Scripts/PlayZone.cs
@@ -5,11 +5,12 @@ using UnityEngine.EventSystems;
 
 public class PlayZone : MonoBehaviour, IDropHandler
 {
+
     public void OnDrop(PointerEventData eventData)
     {
         DragAndDrop obj = eventData.pointerDrag.GetComponent<DragAndDrop>();
         Card card = obj.GetComponent<Card>();
-        if (obj != null && card.cardData.cost < 4 && GameController.instance.manaAmount > 0) 
+        if (obj != null && card.cardData.cost <= GameController.instance.manaAmount && GameController.instance.manaAmount > 0) 
         {
             obj.originalPosition = this.transform;
             GameController.instance.manaAmount -= card.cardData.cost;
@@ -19,5 +20,11 @@ public class PlayZone : MonoBehaviour, IDropHandler
             obj.transform.SetParent(obj.originalPosition);
         }
         Debug.Log("Play Zone");
+        if(card.cardData.cardTitle == "mana")
+        {
+            GameController.instance.manaAmount += 1;
+            Destroy(card.gameObject);
+            GameController.instance.manaPlayed = true;
+        }
     }
 }
